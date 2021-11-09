11b2009edf014cd33c7781516578ceb293e12d06
unknown <martin.arpin@outlook.com>
2021-11-08 17:11:39 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {Projects} from './components/Projects';
+import { Tasks } from './components/Tasks';
+import {BrowserRouter as Router, Route, Switch, useLocation} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { data } from './data/sampleData';
+import GlobalState from './context/GlobalState';
+
+import { AnimatePresence } from 'framer-motion';
+
+//Using React router to navigate between project and task pages. Each task page will use a unique ID and will recieve props from the projectCard component
 
 function App() {
+
+  const getProjects = () => {
+    axios.get('/Projects')
+    .then(response => {
+      setProjects(response.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    getProjects();
+  }, [])
+
+  const [projects, setProjects] = useState([])
+
+  const location = useLocation();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <GlobalState.Provider value ={{projects, setProjects, getProjects}} >
+          <AnimatePresence exitBeforeEnter initial={false}>
+            <Switch location={location} key={location.pathname}>  
+              <Route path='/' exact component={Projects}/>
+              <Route exact path='/:id' 
+                render={(props) => <Tasks {...props}/>}/>
+            </Switch>
+          </AnimatePresence>
+        </GlobalState.Provider>
     </div>
   );
 }
