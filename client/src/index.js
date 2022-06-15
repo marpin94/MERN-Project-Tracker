7e2f1e0c56e48f9b6971f620316727927cec6d51
marpin94 <martin.arpin@outlook.com>
2022-06-15 14:09:01 -0700

    more updates for prod deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <Router>
     <App />
-  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
