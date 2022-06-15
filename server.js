9320cabbd6ea853537fa706bc42e6e9e0d82150a
marpin94 <martin.arpin@outlook.com>
2022-06-15 15:06:26 -0700

    routing updates

--- server.js
+++ server.js
@@ -20,15 +20,33 @@ connection.once('open', () => {
     console.log('MongoDB database connection success')
 })
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-  
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-});
-  
+
+if (process.env.NODE_ENV === "production") {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, "client/build")));
+    // Handle React routing, return all requests to React app
+    app.get("/", function (req, res) {
+      res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
+  }
 
 const projectsRouter = require('./routes/projects.js')
 app.use('/Projects', projectsRouter)
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
+  
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
 
 
 app.listen(port, () => {
