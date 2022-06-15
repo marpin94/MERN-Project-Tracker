6c3da127ae3bf9c1c92e7be364a0c127bd8c733f
marpin94 <martin.arpin@outlook.com>
2022-06-15 13:52:08 -0700

    added path import to server file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 5000;
